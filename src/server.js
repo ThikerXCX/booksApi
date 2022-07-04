c1991582ab699768eb620087e71dea5bfbe1f830
ThikerXCX <rifkimuhammad967@gmail.com>
2022-07-04 21:18:13 +0700

    .

--- src/server.js
+++ src/server.js
@@ -1,16 +1,18 @@
-const Hapi = require('@hapi/hapi');
-const routes = require('./routes');
+const Hapi = require("@hapi/hapi");
+const routes = require("./routes");
 
-const init = async () =>{
-    const server = Hapi.server({
-        port :  process.env.PORT || 5000,
-        //host : 'localhost',
-        //host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
-        routes : {
-            cors : {
-                origin : ['*'],
-            },
-        },
-    });
-}
-init()
+const init = async () => {
+  const server = Hapi.server({
+    port: process.env.PORT || 5000,
+    //host : 'localhost',
+    //host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
+    routes: {
+      cors: {
+        origin: ["*"],
+      },
+    },
+  });
+  server.route(routes);
+  server.start();
+};
+init();
